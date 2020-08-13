eaf0c522e399a1788e670e2491a37a604970bd02
isokissa3 <koti94@windowslive.com>
2020-08-14 01:34:33 +0300

    Add few more PacketReader helpers

---
+++ Net.Buffers/PacketReader.Peek.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.Buffers
+{
+    public ref partial struct PacketReader
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public byte PeekByte() => this.Reader.TryPeek(out byte value) ? value : throw new IndexOutOfRangeException();
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool PeekBool() => this.ReadByte() == 1;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ReadOnlySequence<byte> PeekBytes(long amount) => this.Reader.Sequence.Slice(start: this.Reader.Position, amount);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void PeekBytes(Span<byte> buffer)
+        {
+            if (!this.Reader.TryCopyTo(buffer))
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ Net.Buffers/PacketReader.SequenceEquals.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.Buffers
+{
+    public ref partial struct PacketReader
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool SequenceEqual(ReadOnlySpan<byte> other)
+        {
+            if (!this.TryReadBytes(other.Length, out ReadOnlySequence<byte> sequence))
+            {
+                return false;
+            }
+
+            if (sequence.IsSingleSegment)
+            {
+                return sequence.First.Span.SequenceEqual(other);
+            }
+
+            //Hmm..
+            Span<byte> bytes = other.Length <= 128 
+                ? stackalloc byte[other.Length] 
+                : new byte[other.Length];
+
+            sequence.CopyTo(bytes);
+
+            return bytes.SequenceEqual(other);
+        }
+    }
+}
